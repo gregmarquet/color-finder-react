988596d86fda4d59113238b5f2d788103b23d833
gregmarquet <marquet.greg@gmail.com>
2018-09-11 15:59:09 -0400

    add Question component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Header } from './components/Header/Header';
+import { Question } from './components/Question/Question';
 
 class App extends Component {
   render() {
     return (
       <div className="App main-container">
         <Header />
+        <div id="container" class="container">
+          <form id="colors">
+            <Question />
+          </form>
+        </div>
       </div>
     );
   }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const questions = [
+  {
+    'question-text': 'Sur le plan professionnel vous êtes plutôt :',
+    options: {
+      name: 'question9',
+      value: 'r',
+      id: '1',
+      label: 'Ambitieux'
+    }
+  }
+]
+
+
+function Question(props) {
+
+  return (
+    <div class="questions">
+      <p class="questions-text">{questions[0]['question-text']}</p>
+      <div class="options">
+        <input 
+          type="radio" 
+          name={questions[0].options.name} 
+          value={questions[0].options.value} 
+          id={questions[0].options.id} />
+        <label 
+          for={questions[0].options.id}>
+          {questions[0].options.label}
+        </label>
+      </div>
+      <div class="options">
+        <input type="radio" name="question9" value="j" id="2" />
+        <label for="2">Plein d’idées</label>
+      </div>
+      <div class="options">
+        <input type="radio" name="question9" value="v" id="3" />
+        <label for="3">Méthodique</label>
+      </div>
+      <div class="options">
+        <input type="radio" name="question9" value="b" id="4" />
+        <label for="4">Pointilleux</label>
+      </div>
+    </div>
+  )
+}
+
+export { Question }
