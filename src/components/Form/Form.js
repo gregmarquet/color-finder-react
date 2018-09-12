8d6370d5f0d2196507c9d7d5135e3d454c404c01
gregmarquet <marquet.greg@gmail.com>
2018-09-12 15:57:10 -0400

    add button to Form.js and add style for button and import Form.css in Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Question from '../Question/Question';
+import './Form.css';
 
 function Form() {
   return (
     <div id="container" className="container">
       <form id="colors">
         <Question />
+        <button type="submit">Soumettre</button>
       </form>
     </div>
   );
