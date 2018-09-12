6a4678e0946bf98c397832994fbfb6e2a8855916
gregmarquet <marquet.greg@gmail.com>
2018-09-12 14:36:40 -0400

    add all questions in questions.js

--- src/questions.js
+++ src/questions.js
@@ -3,25 +3,25 @@ const questions = [
     'question-text': 'Sur le plan professionnel vous êtes plutôt :',
     options: [
       {
-        name: 'question9',
+        name: 'question1',
         value: 'r',
         id: '1',
         label: 'Ambitieux'
       },
       {
-        name: 'question9',
+        name: 'question1',
         value: 'j',
         id: '2',
         label: 'Plein d\'idées'
       },
       {
-        name: 'question9',
+        name: 'question1',
         value: 'v',
         id: '3',
         label: 'Méthodique'
       },
       {
-        name: 'question9',
+        name: 'question1',
         value: 'b',
         id: '4',
         label: 'Pointilleux'
@@ -32,28 +32,231 @@ const questions = [
     'question-text': 'Sur le lieu de votre travail ce qui est important pour vous :',
     options: [
       {
-        name: 'question10',
-        value: 'r',
+        name: 'question2',
+        value: 'b',
         id: '5',
         label: 'Réaliser ma mission avec perfection'
       },
       {
-        name: 'question10',
-        value: 'j',
+        name: 'question2',
+        value: 'v',
         id: '6',
         label: 'L\'harmonie dans l\'équipe'
       },
       {
-        name: 'question10',
-        value: 'v',
+        name: 'question2',
+        value: 'j',
         id: '7',
         label: 'Les relations avec les clients'
       },
       {
-        name: 'question10',
-        value: 'b',
+        name: 'question2',
+        value: 'r',
         id: '8',
         label: 'Relever des défis'
+      }
+    ]
+  },
+  {
+    'question-text': 'On dit de vous que vous êtes :',
+    options: [
+      {
+        name: 'question3',
+        value: 'v',
+        id: '9',
+        label: 'Serviable'
+      },
+      {
+        name: 'question3',
+        value: 'b',
+        id: '10',
+        label: 'Discipliné'
+      },
+      {
+        name: 'question3',
+        value: 'j',
+        id: '11',
+        label: 'Convainquant'
+      },
+      {
+        name: 'question3',
+        value: 'r',
+        id: '12',
+        label: 'Déterminé'
+      }
+    ]
+  },
+  {
+    'question-text': 'Vous êtes de nature :',
+    options: [
+      {
+        name: 'question4',
+        value: 'j',
+        id: '13',
+        label: 'Joviale'
+      },
+      {
+        name: 'question4',
+        value: 'r',
+        id: '14',
+        label: 'Impatiente'
+      },
+      {
+        name: 'question4',
+        value: 'b',
+        id: '15',
+        label: 'Perfectionniste'
+      },
+      {
+        name: 'question4',
+        value: 'v',
+        id: '16',
+        label: 'Calme'
+      }
+    ]
+  },
+  {
+    'question-text': 'Vous avez peur :',
+    options: [
+      {
+        name: 'question5',
+        value: 'v',
+        id: '17',
+        label: 'Des conflits'
+      },
+      {
+        name: 'question5',
+        value: 'b',
+        id: '18',
+        label: 'Des critiques'
+      },
+      {
+        name: 'question5',
+        value: 'j',
+        id: '19',
+        label: 'D\'être seul'
+      },
+      {
+        name: 'question5',
+        value: 'r',
+        id: '20',
+        label: 'Que l\'on profite de vous'
+      }
+    ]
+  },
+  {
+    'question-text': 'Vous aimez :',
+    options: [
+      {
+        name: 'question6',
+        value: 'r',
+        id: '21',
+        label: 'Diriger'
+      },
+      {
+        name: 'question6',
+        value: 'v',
+        id: '22',
+        label: 'Aider les autres'
+      },
+      {
+        name: 'question6',
+        value: 'j',
+        id: '23',
+        label: 'Être entouré d\'amis'
+      },
+      {
+        name: 'question6',
+        value: 'b',
+        id: '24',
+        label: 'Approfondir les sujets'
+      }
+    ]
+  },
+  {
+    'question-text': 'Choisissez un mot :',
+    options: [
+      {
+        name: 'question7',
+        value: 'j',
+        id: '25',
+        label: 'Improvisation'
+      },
+      {
+        name: 'question7',
+        value: 'r',
+        id: '26',
+        label: 'Action'
+      },
+      {
+        name: 'question7',
+        value: 'b',
+        id: '27',
+        label: 'Précision'
+      },
+      {
+        name: 'question7',
+        value: 'v',
+        id: '28',
+        label: 'Écoute'
+      }
+    ]
+  },
+  {
+    'question-text': 'Vous savez :',
+    options: [
+      {
+        name: 'question8',
+        value: 'r',
+        id: '29',
+        label: 'Décider'
+      },
+      {
+        name: 'question8',
+        value: 'b',
+        id: '30',
+        label: 'Analyser'
+      },
+      {
+        name: 'question8',
+        value: 'v',
+        id: '31',
+        label: 'Vous adapter'
+      },
+      {
+        name: 'question8',
+        value: 'j',
+        id: '32',
+        label: 'Motiver'
+      }
+    ]
+  },
+  {
+    'question-text': 'Vous pouvez être :',
+    options: [
+      {
+        name: 'question9',
+        value: 'r',
+        id: '33',
+        label: 'Trop direct'
+      },
+      {
+        name: 'question9',
+        value: 'b',
+        id: '34',
+        label: 'Froid'
+      },
+      {
+        name: 'question9',
+        value: 'j',
+        id: '35',
+        label: 'Dissipé'
+      },
+      {
+        name: 'question9',
+        value: 'v',
+        id: '36',
+        label: 'Possessif'
       }
     ]
   }
